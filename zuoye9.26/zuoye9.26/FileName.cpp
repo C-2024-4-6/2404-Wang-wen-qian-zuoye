a1ebbd1cdb80f79bfbfaf4ef3db0c57c9d4c298e
saki-suki <19806751216@139.com>
2024-11-04 13:35:33 +0800

    first

---
+++ zuoye9.26/zuoye9.26/FileName.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	/*int a(1), b(10);
+	do
+	{
+		b -= a; ++a;
+	} while (b-- < 0);
+	cout << a << b << endl;
+	return 0;
+	*/
+	/*int x(1), a(0), b(0);
+	switch (x)
+	{
+	case 0:b++;
+	case 1:a++;
+	case 2:a++; b++;
+	}
+	cout << a << '\n' << b << endl;
+	
+	int i(0);
+	while (i<10)
+	{
+		if (i < 1)continue;
+		if (i == 5) break;
+		i++;
+	}
+	int a(1),b(1);
+	for (; a <= 100; a++) 
+	{
+		if (b >= 10)break;
+		if (b % 3 == 1) 
+		{
+			b += 3; continue;
+		}
+	}
+	cout << a << endl;*/
+	char c = '\78';
+		cout << c << endl;
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+
+
+
+
+}
